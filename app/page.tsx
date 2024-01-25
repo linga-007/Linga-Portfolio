53b3ae22547afe739867e88bb34fa5d4816caa48
linga-007 <lingesh480500@gmail.com>
2024-01-26 00:41:43 +0530

    intro page completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
 import Header from "./ui/header";
 import Intro from "./ui/intro";
-
+import Education from "./ui/education";
 export default function Home() {
   return (
     <>
     <Header/>
     <Intro/>
-    helo
+    <Education/>
     </>
   );
 }
--- app/ui/intro.tsx
+++ app/ui/intro.tsx
@@ -1,15 +1,35 @@
 import React from 'react'
+import Image from 'next/image'
+import linga from "./lingaimg.png"
 
 const intro = () => {
   return (
-    <div className='flex justify-between mx-32 p-12 pt-16 h-[650px] w-full '>
-      <div className='flex text-slate-800 gap-8 flex-col '>
+    <div className='flex justify-between mx-32 p-12  h-[650px] '>
+      <div className='flex text-slate-800 gap-8 pt-16 w-[40%] flex-col '>
         <div className='text-black font-bold text-5xl gap-5 flex flex-col'>
           <h2>Hello, I'm</h2>
           <h2>Lingesh a Coder,</h2>
           <h3>and Developer</h3>
         </div>
-        <div>A dedicated Developer with a passion for creating digital experiences that not only look beautiful but also function seamlessly and a Coder </div>
+        <div>A dedicated Developer with a passion for creating digital experiences that not only look beautiful but also funtion seemlessly.</div>
+        <button className='text-white w-32 h-12 rounded-3xl bg-black'>Get Started</button>
+      </div>
+      <div className='pr-10'>
+        <Image src={linga} width={450} height={450} alt='img' className='rounded-full bg-[#FFB321]'></Image>
+      </div>
+      <div className='flex flex-col w-[15%] gap-14'>
+        <div className='pt-16 '>
+          <h2 className='text-black font-semibold text-3xl flex justify-end '>12</h2>
+          <h2 className='text-zinc-400 text-lg font-semibold flex justify-end '>Years of Experience </h2>
+        </div>
+        <div className=''>
+          <h2 className='text-black font-semibold text-3xl flex justify-end '>2</h2>
+          <h2 className='text-zinc-400 text-lg font-semibold flex justify-end '>Currently Working Projects </h2>
+        </div>
+        <div className=''>
+          <h2 className='text-black font-semibold text-3xl flex justify-end '>2</h2>
+          <h2 className='text-zinc-400 text-lg font-semibold flex justify-end '>Currently Working Projects </h2>
+        </div>
       </div>
       
     </div>
