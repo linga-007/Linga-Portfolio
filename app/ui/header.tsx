6ea163be60678d4a131a820b1608edb8572b0ca2
linga-007 <lingesh480500@gmail.com>
2024-01-26 00:15:45 +0530

    completed intro page

--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -8,11 +8,11 @@ const header = () => {
         
         <h1 className='text-4xl font-semibold font-mono text-slate-800'>Lingesh</h1>
         <div className='flex justify-between text-slate-800 gap-12 font-medium text-lg'>
-            <button className='hover:text-orange-500'>Home</button >
-            <button className='hover:text-orange-500'>About</button >
-            <button className='hover:text-orange-500'>Service</button >
-            <button className='hover:text-orange-500'>Portfolio</button >
-            <button className='hover:text-orange-500'>Contact</button >
+            <button className='hover:text-[#FFB321]'>Home</button >
+            <button className='hover:text-[#FFB321]'>About</button >
+            <button className='hover:text-[#FFB321]'>Service</button >
+            <button className='hover:text-[#FFB321]'>Portfolio</button >
+            <button className='hover:text-[#FFB321]'>Contact</button >
         </div>
 
         <button className='bg-transparent border border-black  rounded-3xl font-semibold text-black w-32 text-lg h-12'>Contact</button>
